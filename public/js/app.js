5d07faf933c7ee248128135b076c48bc04629918
Jake Stewart <jstewart@vaonet.com>
2022-08-04 11:48:39 -0500

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded')
-
 const weatherForm = document.querySelector('form')
 const searchTerm = document.querySelector('input')
 const p1 = document.getElementById('1')
