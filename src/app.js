e05d465cc61d126e50f6b667069377cbdd74eefd
Jake Stewart <jstewart@vaonet.com>
2022-08-04 12:39:46 -0500

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -48,22 +49,25 @@ app.get('/weather', (req, res) => {
     })
     return
   }
-  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-    if (error) {
-      return res.send({ error })
-    }
-
-    forecast(latitude, longitude, (error, forecastData) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
-        return ressend({ error })
+        return res.send({ error })
       }
-      res.send({
-        forecast: forecastData,
-        location: location,
-        address: req.query.address,
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return ressend({ error })
+        }
+        res.send({
+          forecast: forecastData,
+          location: location,
+          address: req.query.address,
+        })
       })
-    })
-  })
+    }
+  )
 })
 
 app.get('/products', (req, res) => {
@@ -97,6 +101,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.')
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`)
 })
